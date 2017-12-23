56080e6c815879da4f0968c1e8928047140305a2
赵淑楷 <zhaoshukai@xdf.cn>
2017-12-23 16:23:02 +0800

    优惠类Discount

---
+++ src/ExperienceTDD/Discount.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhaoshukai
+ * Date: 2017-12-23
+ * Time: 12:09
+ */
+namespace ExperienceTDD;
+
+class Discount
+{
+    static function getPrice($price) {
+        if($price >= 100) {
+            $price -= 50;
+        }
+
+        return $price;
+    }
+
+}
